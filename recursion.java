2de52a5ae757b7e55a6118b5016fd15457af3ca3
SujanThapa <158448355+sujanthapa01@users.noreply.github.com>
2026-09-05 00:26:04 +0530

    learning recursion

---
+++ recursion.java
@@ -0,0 +1,52 @@
+
+class Main {
+    public static void printNum(int n){
+
+        System.out.println(n);
+
+        if(n == 5){
+            return ;
+        }
+
+        printNum(n + 1);
+    }
+
+
+    public static void addSum(int i , int n, int finalsum){
+
+
+        if(i == n){
+            finalsum += i ;
+            System.out.println(finalsum);
+            return;
+        }
+
+        finalsum += i;
+
+        addSum(i + 1,n, finalsum);
+
+
+
+    }
+
+    public static int factorial(int n ){
+
+        if(n == 1){
+            return 1;
+        }
+
+        int fac =  factorial(n - 1);
+        int result = n * fac ;
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Start small. Ship something.");
+
+        // printNum(0);
+        // addSum(1, 100,0);
+        int result = factorial(200);
+        System.out.println(result);
+    }
+}
