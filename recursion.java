bd406568421e5e1e70933f5d15aed502d4959327
SujanThapa <158448355+sujanthapa01@users.noreply.github.com>
2026-09-05 12:18:04 +0530

    add fibonacci series print function using recursion

--- recursion.java
+++ recursion.java
@@ -30,7 +30,6 @@ class Main {
     }
 
     public static int factorial(int n ){
-
         if(n == 1){
             return 1;
         }
@@ -38,6 +37,17 @@ class Main {
         int fac =  factorial(n - 1);
         int result = n * fac ;
         return result;
+    }
+
+    public static void fabonachi(int value1, int value2, int n){
+
+        if(n == 0){
+            return;
+        }
+
+        int value3 = value1 + value2;
+        System.out.println(value3);
+        fabonachi(value2,value3,n-1);
 
     }
 
@@ -46,7 +56,12 @@ class Main {
 
         // printNum(0);
         // addSum(1, 100,0);
-        int result = factorial(200);
-        System.out.println(result);
+//        int result = factorial(200);
+//        System.out.println(result);
+
+        int n = 20;
+        System.out.println(0);
+        System.out.println(1);
+        fabonachi(0,1,n-2);
     }
 }
